0c38a4f5b37d3df4dfa54cdd20183791aa1f74f5
PedroCaurio <cauriopedro2@gmail.com>
2025-11-11 16:17:35 -0300

    att no main

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "soma.h"
 #include "subtrai.h"
+#include "multiplica.h"
+#include "divide.h"
 
 int main(void)
 {
-    int s;
+    int s, sub, mul, div;
     s = soma(4, 6);
-    s = subtrai(s, 7);
-    printf("s = %d\n", s);
+    sub = subtrai(10, 7);
+    mul = multiplica(2, 2);
+    div = divide(10, 5);
+    printf("soma = %d\nsubtracao = %d\nmultiplicacao = %d\ndivisao = %d", s, sub, mul, div);
 
     return 0;
 }
